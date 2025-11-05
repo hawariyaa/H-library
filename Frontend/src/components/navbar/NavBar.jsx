f25e357685b87fc4233f3b53424bd2e9811a0e51
hawariyaw paulos <hawipaul308@gmail.com>
2025-11-05 18:50:07 +0300

    Front-end of Hlibrary

---
+++ Frontend/src/components/navbar/NavBar.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import './navbar.css'
+import searchb from '../../assets/search-b.png'
+import searchw from '../../assets/search-w.png'
+import day from '../../assets/day.png'
+import night from '../../assets/night.png'
+import menub from '../../assets/menuicon-b.png'
+import menuw from '../../assets/menuicon-w.png'
+
+function NavBar() {
+    const [search, setsearch] = useState(false)
+    const handlesearch = () =>{
+        setsearch(!search)
+    }
+  return (
+    <div className='nav-bar'>
+        <div className="logo">
+          <h3>H-library</h3>
+          <select name="language" id="">
+            <option value="English">English</option>
+            <option value="Amharic">አማርኛ</option>        
+          </select>
+        </div>
+        <div className={`right-side ${search}`}>
+            <i class="fa-solid fa-share-nodes"></i>
+            <img src={night} alt="modeicon" />
+            <input type="text" placeholder='search for book name, author...'/> 
+            <i class="fa-solid fa-magnifying-glass" onClick={handlesearch}></i>
+            <p>Login/sign-up</p>
+            <i class="fa-solid fa-bars"></i>
+        </div>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ Frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './home.css'
+import NavBar from '../../components/navbar/NavBar'
+
+function Home() {
+  return (
+    <div>
+        <NavBar />
+    </div>
+  )
+}
+
+export default Home
