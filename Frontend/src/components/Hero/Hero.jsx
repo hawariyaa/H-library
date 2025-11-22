2a7e0dd0e8fc17c0aef136ad1a8eace4dc327bfb
hawariyaw paulos <hawipaul308@gmail.com>
2025-11-22 20:51:56 +0300

    hero-animation

--- Frontend/src/components/Hero/Hero.jsx
+++ Frontend/src/components/Hero/Hero.jsx
@@ -1,13 +1,27 @@
 import React, { useContext } from 'react'
 import './hero.css'
 import { DarkMode } from '../../context/DarkMode'
+import gsap from "gsap"  
+import {useGSAP} from "@gsap/react"
+import { SplitText } from 'gsap/all'
 
 function Hero() {
   const {Dark} = useContext(DarkMode)
+  gsap.registerPlugin(SplitText)
+  useGSAP(()=>{
+    const heroSplit = new SplitText('.right-hero h1', {type: 'chars'})
+    gsap.from(heroSplit.chars, {
+      opacity:0,
+      duration: 2,
+      repeat:-1,
+      repeatDelay:2,
+      stagger:0.3
+    })
+  })
   return (
     <div className={`hero ${Dark}`}>
          <div className='right-hero'>
-              <h1>H-LIBRARY</h1>
+              <h1>H-LI<span>B</span>RARY</h1>
               <h4>"Books are the training weights of the mind." - Epictetus</h4>
          </div>
          <div className={`left-hero ${Dark}`}>
